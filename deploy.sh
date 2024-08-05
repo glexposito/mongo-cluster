c1140a45e7ea2e5780988b869ef5f966a6805546
Guillermo <glexposito@gmail.com>
2024-08-06 10:07:27 +1200

    Refactor the project to use a single Docker Compose file

--- deploy.sh
+++ deploy.sh
@@ -25,22 +25,8 @@ typewriter "Docker network was successfully created"
 echo
 
 # Print a message indicating the start of MongoDB sharded cluster configuration
-typewriter "Configuring MongoDB Sharded Clusters with Replica Sets"
-
-# Configure Shard 1 using the Docker Compose file for shard1
-typewriter "Starting Shard 1 (mongo-shard1)..."
-# docker compose -f shard1/docker-compose.yaml up -d
-docker run -d -p 27101:27017 --name mongo-shard1-1 --network mongodb-network mongo:7 mongod --shardsvr --replSet mongo-shard1-rs --port 27017 --bind_ip localhost,mongo-shard1-1
-docker run -d -p 27102:27017 --name mongo-shard1-2 --network mongodb-network mongo:7 mongod --shardsvr --replSet mongo-shard1-rs --port 27017 --bind_ip localhost,mongo-shard1-2
-docker run -d -p 27103:27017 --name mongo-shard1-3 --network mongodb-network mongo:7 mongod --shardsvr --replSet mongo-shard1-rs --port 27017 --bind_ip localhost,mongo-shard1-3
-
-# Configure Shard 2 using the Docker Compose file for shard2
-typewriter "Starting Shard 2 (mongo-shard2)..."
-#docker compose -f shard2/docker-compose.yaml up -d
-docker run -d -p 27201:27017 --name mongo-shard2-1 --network mongodb-network mongo:7 mongod --shardsvr --replSet mongo-shard2-rs --port 27017 --bind_ip localhost,mongo-shard2-1
-docker run -d -p 27202:27017 --name mongo-shard2-2 --network mongodb-network mongo:7 mongod --shardsvr --replSet mongo-shard2-rs --port 27017 --bind_ip localhost,mongo-shard2-2
-docker run -d -p 27203:27017 --name mongo-shard2-3 --network mongodb-network mongo:7 mongod --shardsvr --replSet mongo-shard2-rs --port 27017 --bind_ip localhost,mongo-shard2-3
-echo
+typewriter "Configuring MongoDB Clusters with Replica Sets"
+docker compose up -d
 
 # Checking connectivity with improved readability and error handling
 typewriter "Checking connectivity..."
@@ -71,19 +57,6 @@ check_connectivity() {
   exit 1
 }
 
-# Check connectivity for Shard 1
-check_connectivity "mongo-shard1-1" 27017
-check_connectivity "mongo-shard1-2" 27017
-check_connectivity "mongo-shard1-3" 27017
-
-# Check connectivity for Shard 2
-check_connectivity "mongo-shard2-1" 27017
-check_connectivity "mongo-shard2-2" 27017
-check_connectivity "mongo-shard2-3" 27017
-
-typewriter "All instances are reachable and running."
-echo
-
 # Initialize the replica set for Shard 1
 typewriter "Initializing the Replica Sets..."
 typewriter "Replica Set 1 (mongo-shard1-rs)"
@@ -111,23 +84,6 @@ docker exec mongo-shard2-1 mongosh --eval "rs.initiate({
 })"
 echo
 
-# Set up the Config server replica set
-typewriter "Setting up Config Server Replica Set..."
-docker run -dit --name mongo-config-server-1 --net mongodb-network -p 27001:27017 mongo:7 --configsvr --replSet mongo-config-server-rs --port 27017 --bind_ip localhost,mongo-config-server-1
-docker run -dit --name mongo-config-server-2 --net mongodb-network -p 27002:27017 mongo:7 --configsvr --replSet mongo-config-server-rs --port 27017 --bind_ip localhost,mongo-config-server-2
-docker run -dit --name mongo-config-server-3 --net mongodb-network -p 27003:27017 mongo:7 --configsvr --replSet mongo-config-server-rs --port 27017 --bind_ip localhost,mongo-config-server-3
-#docker compose -f config/docker-compose.yaml up -d
-echo
-
-# Check connectivity for Config Servers
-typewriter "Checking connectivity..."
-check_connectivity "mongo-config-server-1" 27017
-check_connectivity "mongo-config-server-2" 27017
-check_connectivity "mongo-config-server-3" 27017
-
-typewriter "All instances are reachable and running."
-echo
-
 # Initialize the replica set
 typewriter "Initializing the Config Server Replica Set..."
 docker exec -it mongo-config-server-1 mongosh --port 27017 --eval "rs.initiate({                                                                                                 
@@ -140,15 +96,23 @@ docker exec -it mongo-config-server-1 mongosh --port 27017 --eval "rs.initiate({
 })"
 echo
 
-# Set up mongos
-typewriter "Setup mongos router"
-# docker compose -f router/docker-compose.yaml up -d
-docker run -dit --name mongos-router-1 --net mongodb-network -p 27100:27017 mongo:7 mongos --configdb mongo-config-server-rs/mongo-config-server-1:27017,mongo-config-server-2:27017,mongo-config-server-3:27017 --port 27017 --bind_ip localhost,mongos-router-1
-docker run -dit --name mongos-router-2 --net mongodb-network -p 27200:27017 mongo:7 mongos --configdb mongo-config-server-rs/mongo-config-server-1:27017,mongo-config-server-2:27017,mongo-config-server-3:27017 --port 27017 --bind_ip localhost,mongos-router-2
-echo
+typewriter "Checking connectivity..."
+# Check connectivity for Shard 1
+check_connectivity "mongo-shard1-1" 27017
+check_connectivity "mongo-shard1-2" 27017
+check_connectivity "mongo-shard1-3" 27017
+
+# Check connectivity for Shard 2
+check_connectivity "mongo-shard2-1" 27017
+check_connectivity "mongo-shard2-2" 27017
+check_connectivity "mongo-shard2-3" 27017
+
+# Check connectivity for Config Servers
+check_connectivity "mongo-config-server-1" 27017
+check_connectivity "mongo-config-server-2" 27017
+check_connectivity "mongo-config-server-3" 27017
 
 # Check connectivity for mongos router
-typewriter "Checking connectivity..."
 check_connectivity "mongos-router-1" 27017
 check_connectivity "mongos-router-2" 27017
 
